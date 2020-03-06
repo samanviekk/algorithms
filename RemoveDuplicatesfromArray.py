f20174aec58e809786090a55138327726de6d8a5
samanviekk <samanviekk@gmail.com>
2020-03-05 16:44:42 -0800

    implemented 2 ways of remove duplicates in a sorted array

--- RemoveDuplicatesfromArray.py
+++ RemoveDuplicatesfromArray.py
@@ -4,22 +4,35 @@ def remove_duplicates(arr):
     while curr < len(arr):
         if arr[curr] == arr[pre]:
             curr = curr + 1
-
-        else:
-            pre = pre + 2
+        elif (curr - pre) > 1:
+            pre = pre + 1
             arr[pre] = arr[curr]
             curr = curr + 1
-            pre = pre + 1
         else:
             curr = curr + 1
             pre = pre + 1
+    del arr[pre+1:]
+    return arr
 
-    return len(arr)
 
+def removeDuplicates(nums):
+    if not nums:
+        return 0
+
+    k = 1
+    for i in range(1, len(nums)):
+        if nums[k - 1] != nums[i]:
+            nums[k] = nums[i]
+            k = k + 1
+    del nums[k:]
+    return k
 
 testarr = [0, 0, 0, 0, 1, 3, 4]
-res = remove_duplicates(testarr)
-print(res)
+remove_duplicates(testarr)
 print(testarr)
+
+A = [0, 0, 0, 0, 1, 3, 4]
+removeDuplicates(A)
+print(A)
 
 
